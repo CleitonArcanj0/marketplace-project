d60d24e812ade68802d50ca1477933077c8efd48
Cleiton Arcanjo <152240025+CleitonArcanj0@users.noreply.github.com>
2026-04-16 22:56:49 -0300

    feat: adiciona busca em tempo real com debounce e integração com API

--- MarketHub/Js/api.js
+++ MarketHub/Js/api.js
@@ -1,4 +1,5 @@
-const conteiner = document.querySelector(".conteiner")
+const conteiner = document.querySelector(".conteiner"),
+    pesquisa = document.querySelector(".pesquisarProdutos")
 
 async function buscarProdutos() {
     try {
@@ -10,14 +11,14 @@ async function buscarProdutos() {
 
         console.log(dados)
 
-        let html =''
+        let html = ''
         dados.products.forEach(element => {
             html += `
              <div class="card_produto">
-            <img src="${element.thumbnail}" alt="img" class="imagem_produto">
-            <h3 class="titulo_produto">${element.title}</h3>
-            <p class="descricao">${element.description}</p>
-            <p class="preco">R$ ${element.price.toFixed(2)}</p>
+                <img src="${element.thumbnail}" alt="img" class="imagem_produto">
+                <h3 class="titulo_produto">${element.title}</h3>
+                <p class="descricao">${element.description}</p>
+                <p class="preco">R$ ${element.price.toFixed(2)}</p>
             </div>
             `
         });
@@ -30,5 +31,45 @@ async function buscarProdutos() {
 
 }
 
+let time = null
+pesquisa.addEventListener("keyup", () => {
+    clearTimeout(time)
+
+    time = setTimeout(() => {
+        pesquisarProdutos()
+    }, 500)
+})
+
+async function pesquisarProdutos() {
+    const produto = pesquisa.value
+    try {
+        const resposta = await fetch(`https://dummyjson.com/products/search?q=${produto}`)
+        if (!resposta.ok) {
+            console.log("Erro na requisição");
+        }
+        const dados = await resposta.json()
+
+        if (dados.products.length == 0) {
+            conteiner.innerHTML = `<p>Nenhum produto encontrado!</p>`
+            return
+        }
+
+
+        let html = dados.products.map(p => {
+            return `
+            <div class="card_produto">
+                    <img src="${p.thumbnail}" alt="img" class="imagem_produto">
+                    <h3 class="titulo_produto">${p.title}</h3>
+                    <p class="descricao">${p.description}</p>
+                    <p class="preco">R$ ${p.price.toFixed(2)}</p>
+            </div>`;
+        }).join("");
+
+        conteiner.innerHTML = html
+
+    } catch (error) {
+        console.log(`Erro ao buscar dados: ${error}`)
+    }
+}
 
 buscarProdutos()
